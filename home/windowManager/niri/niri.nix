5242c8596449351ab690241174dca72f52888818
Sudhir Krisna <sudhboiii@gmail.com>
2026-04-27 21:45:29 -0300

    k

--- home/windowManager/niri/niri.nix
+++ home/windowManager/niri/niri.nix
@@ -2,7 +2,7 @@
 {
   imports = [ inputs.niri.homeModules.niri ];
 
-  home.file.".config/niri/niri.kdl".source = ./niri.kdl;
+  home.file.".config/niri/config.kdl".source = ./config.kdl;
   home.file.".config/niri/scripts/niri_tile_to_n.py".source = ./scripts/niri_tile_to_n.py;
   home.file.".config/niri/modules".source = ./modules;
 }
