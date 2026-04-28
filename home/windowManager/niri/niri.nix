214931c86ed81a1159bbcbe2a1f3d4cac0134690
Sudhir Krisna <sudhboiii@gmail.com>
2026-04-27 21:36:13 -0300

    a

--- home/windowManager/niri/niri.nix
+++ home/windowManager/niri/niri.nix
@@ -2,10 +2,7 @@
 {
   imports = [ inputs.niri.homeModules.niri ];
 
-  programs.niri.config = ''
-    include "modules/animations.kdl"
-  '';
-
+  home.file.".config/niri/niri.kdl".source = ./niri.kdl;
   home.file.".config/niri/scripts/niri_tile_to_n.py".source = ./scripts/niri_tile_to_n.py;
   home.file.".config/niri/modules".source = ./modules;
 }
